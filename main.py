cb878b3c3771cca2c7bb0fd41c0ac599464307bd
kumitatepazuru <riku.ueda@outlook.jp>
2021-03-23 20:11:14 +0900

    お遊び統計bot?

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import sys
@@ -39,9 +40,56 @@ class main(commands.Bot):
                 channel = self.get_channel(int(f.read().splitlines()[0]))
                 await channel.send("restarted. command completed.")
             os.remove("ID_DISCORD_CL")
-        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='v 2.0.3'
+        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='v 2.1.0'
                                                                                                       '/powered by '
                                                                                                       'Riku Ueda'))
+        self.toukei["on_ready"] += 1
+        self.save()
+
+    async def on_error(self, *args, **kwargs):
+        self.toukei["on_error"] += 1
+        self.save()
+
+    async def on_typing(self, *args, **kwargs):
+        self.toukei["on_typing"] += 1
+        self.save()
+
+    async def on_message(self, *args, **kwargs):
+        self.toukei["on_message"] += 1
+        self.save()
+
+    async def on_raw_message_delete(self, *args, **kwargs):
+        self.toukei["on_raw_message_delete"] += 1
+        self.save()
+
+    async def on_raw_message_edit(self, *args, **kwargs):
+        self.toukei["on_raw_message_edit"] += 1
+        self.save()
+
+    async def on_raw_reaction_add(self, *args, **kwargs):
+        self.toukei["on_raw_reaction_add"] += 1
+        self.save()
+
+    async def on_raw_reaction_remove(self, *args, **kwargs):
+        self.toukei["on_raw_reaction_remove"] += 1
+        self.save()
+
+    async def on_raw_reaction_clear(self, *args, **kwargs):
+        self.toukei["on_raw_reaction_clear"] += 1
+        self.save()
+
+    async def on_guild_channel_pins_update(self, *args, **kwargs):
+        self.toukei["on_guild_channel_pins_update"] += 1
+        self.save()
+
+    def save(self):
+        logging.info(self.toukei)
+        if self.toukei is None:
+            self.toukei = {"on_ready": 0, "on_error": 0, "on_typing": 0, "on_message": 0, "on_raw_message_delete": 0,
+                           "on_raw_message_edit": 0, "on_raw_reaction_add": 0, "on_raw_reaction_remove": 0,
+                           "on_raw_reaction_clear": 0, "on_guild_channel_pins_update": 0}
+        with open("./data/toukei.json", "w") as f:
+            json.dump(self.toukei, f)
 
 
 # MyBotのインスタンス化及び起動処理。
